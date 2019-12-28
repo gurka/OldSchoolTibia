19e3619a4e17e6cc39aa06c7058eb5a57a286c14
Simon Sandström <simon@nikanor.nu>
2019-12-28 21:04:06 +0100

    Use OBS hotkeys and make scripts executable

--- tibiareplay/src/tibia-replay-dll/utils/obs.cc
+++ tibiareplay/src/tibia-replay-dll/utils/obs.cc
@@ -1,5 +1,6 @@
 #include "obs.h"
 
+#include "keystroke.h"
 #include <windows.h>
 
 namespace Obs
@@ -7,37 +8,13 @@ namespace Obs
 
 bool startRecording()
 {
-  HWND hWndOBS = FindWindow("OBSWindowClass", NULL);
-  if (hWndOBS == NULL)
-  {
-    return false;
-  }
-
-  HWND hWndStartRecording = FindWindowEx(hWndOBS, NULL, "Button", "Start Recording");
-  if (hWndStartRecording == NULL)
-  {
-    return false;
-  }
-
-  SendMessage(hWndStartRecording, BM_CLICK, 0, 0);
+  KeyStroke::pressAndRelease({ VK_CONTROL, VK_HOME });
   return true;
 }
 
 bool stopRecording()
 {
-  HWND hWndOBS = FindWindow("OBSWindowClass", NULL);
-  if (hWndOBS == NULL)
-  {
-    return false;
-  }
-
-  HWND hWndStopRecording = FindWindowEx(hWndOBS, NULL, "Button", "Stop Recording");
-  if (hWndStopRecording == NULL)
-  {
-    return false;
-  }
-
-  SendMessage(hWndStopRecording, BM_CLICK, 0, 0);
+  KeyStroke::pressAndRelease({ VK_CONTROL, VK_END });
   return true;
 }
 
