210cb2950d6fc895d2747e5731f30e92154e848e
Simon Sandström <simon@nikanor.nu>
2024-08-20 23:15:55 +0200

    convert.py: convert in parallel

--- tools/convert.py
+++ tools/convert.py
@@ -1,9 +1,26 @@
 #!/usr/bin/env python3
 import argparse
+import concurrent.futures
 import os
+import signal
 import sys
+import threading
+import time
 
 from libs import recording
+
+
+def terminate_if_parent_dies(parent_pid):
+    pid = os.getpid()
+    def check_parent():
+        while True:
+            try:
+                os.kill(parent_pid, 0)
+            except OSError:
+                os.kill(pid, signal.SIGTERM)
+            time.sleep(1)
+    thread = threading.Thread(target=check_parent, daemon=True)
+    thread.start()
 
 
 def convert_file(filename, force, version, overwrite, delete, output_dir):
@@ -28,7 +45,14 @@ def convert_file(filename, force, version, overwrite, delete, output_dir):
         subfolder_dir = os.path.join(output_dir, tmp[0] + '.' + tmp[1:])
         if not os.path.isdir(subfolder_dir):
             print("'{}' does not exist, creating directory.".format(subfolder_dir))
-            os.mkdir(subfolder_dir)
+            try:
+                os.mkdir(subfolder_dir)
+            except FileExistsError:
+                # This can happen when converting in parallel,
+                # if two threads (processes) tries to create
+                # the same directory at the same time
+                pass
+
         output = os.path.join(subfolder_dir, output_filename)
     else:
         output = os.path.join(output_dir, output_filename)
@@ -57,6 +81,7 @@ if __name__ == '__main__':
     parser.add_argument("-v", "--version", help="use this version if no version was automatically detected. To always set the given version, use -o/--overwrite.", type=int)
     parser.add_argument("-o", "--overwrite", help="always use the version provided with -v/--version, even if a version was automatically detected.", action='store_true')
     parser.add_argument("-d", "--delete", help="delete source file if it was converted successfully.", action='store_true')
+    parser.add_argument("-j", "--jobs", help="convert files in parallel using this many workers.", type=int, default=1)
     parser.add_argument("OUTPUT_DIR", help="output files will be placed in this directory.")
     parser.add_argument("FILE", help="file(s) to convert or directory to scan for files", nargs='+')
     args = parser.parse_args()
@@ -66,6 +91,7 @@ if __name__ == '__main__':
     version = args.version
     overwrite = args.overwrite
     delete = args.delete
+    jobs = args.jobs
     output_dir = args.OUTPUT_DIR
     filenames = args.FILE
 
@@ -85,22 +111,23 @@ if __name__ == '__main__':
         print("'{}' does not exist, creating directory.".format(output_dir))
         os.mkdir(output_dir)
 
-    num_processed = 0
-    num_converted = 0
-
+    filenames_to_process = []
     for filename in filenames:
         if os.path.isdir(filename):
             for current_dir, _, current_filenames in os.walk(filename):
-                for current_filename in current_filenames:
-                    num_processed += 1
-                    if convert_file(os.path.join(current_dir, current_filename), force, version, overwrite, delete, output_dir):
-                        num_converted += 1
-
+                filenames_to_process += [os.path.join(current_dir, filename) for filename in current_filenames]
         else:
-            num_processed += 1
-            if convert_file(filename, force, version, overwrite, delete, output_dir):
+            filenames_to_process.append(filename)
+
+    num_converted = 0
+    with concurrent.futures.ProcessPoolExecutor(max_workers=jobs, initializer=terminate_if_parent_dies, initargs=(os.getpid(), )) as executor:
+        futures = {
+            executor.submit(convert_file, filename, force, version, overwrite, delete, output_dir): filename for filename in filenames_to_process
+        }
+        for future in concurrent.futures.as_completed(futures):
+            if future.result():
                 num_converted += 1
 
-    print(f'Number of files processed: {num_processed}')
+    print(f'Number of files processed: {len(filenames_to_process)}')
     print(f'Number of files converted: {num_converted}')
-    print(f'Number of files failed: {num_processed - num_converted}')
+    print(f'Number of files failed: {len(filenames_to_process) - num_converted}')
